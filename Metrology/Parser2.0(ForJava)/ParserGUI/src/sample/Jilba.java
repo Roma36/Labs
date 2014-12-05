acc8ad5d413c0322a00edfa1113d7a3375a7adf9
Roma36 <romagrin36@gmail.com>
2014-12-05 15:47:07 +0200

    Metrology. Lab2(Sloc+&Jilba) Metrics. Java

---
+++ Metrology/Parser2.0(ForJava)/ParserGUI/src/sample/Jilba.java
@@ -0,0 +1,185 @@
+package sample;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Роман on 07.11.2014.
+ */
+public class Jilba extends Metrics {
+    private Comments comments;
+    private int numberOfOperators = 0;
+    private int CL = 0;
+    private int arithmeticOperatorCount = 0;
+    private int compoundAssignmentOperatorCount = 0;
+    private int comparisonOperatorCount = 0;
+    private int logicalOperatorCount = 0;
+    private int cyclicOperatorCount = 0;
+    private int otherOperatorCount = 0;
+    private int maxDepth = 0;
+    public String allCodeInline="";
+    private Pattern patternCL = Pattern.compile("([ \t]*if([ \t]|[(])|[?].+?[:].+)");
+    private Pattern ifCLMatchPattern = Pattern.compile("[ \t]*if[ \t]*[(](.){1,}[)](.)*");
+    private Pattern braceMatchPattern = Pattern.compile("[ \t]*[{](.)*");
+
+    private Pattern arithmeticOperatorPattern = Pattern.compile("[a-zA-Z \t0-9)](=|[+]|-|[*]|[/]|%|[+][+]|[-][-])[a-zA-Z \t0-9(;]");
+    private Pattern compoundAssignmentOperatorPattern = Pattern.compile("(([-+*/%&|^])|<{2}|>{2})=(?!=)");
+    private Pattern comparisonOperatorPattern = Pattern.compile("[a-zA-Z \t0-9)](==|!=|>|<|>=|<=)[a-zA-Z \t0-9(;]");
+    private Pattern logicalOperatorPattern = Pattern.compile("(![a-zA-Z \t(]|[a-zA-Z \t0-9)](&&|[|][|])[a-zA-Z \t0-9(;])");
+    private Pattern cyclicOperatorPattern = Pattern.compile("((for|while)[ \t]*[(]|do[ \t]*[{])");
+    private Pattern otherOperatorsPattern = Pattern.compile("(![a-zA-Z \t(]|[a-zA-Z \t)](~|&|\\||^|<<|>>|->|\\.|->\\*|\\.\\*|\\[|,|sizeof|alignof|typeid)[a-zA-Z \t0-9(;])");
+
+    private ArrayList<String> stringsWithoutComments = new ArrayList<String>();
+    protected ArrayList<String> sourceCodeStringsOnly = new ArrayList<String>();
+
+    public Jilba(){
+        comments = super.comments;
+        stringsWithoutComments = super.stringsWithoutComments;
+        sourceCodeStringsOnly = super.sourceCodeStringsOnly;
+        for(String currentString:stringsWithoutComments){
+            allCodeInline+=currentString;
+        }
+        countOperators();
+    }
+
+    public int getCL(){
+        return CL;
+    }
+
+    public float getRelativeCL(){
+        return (float)CL/(float)numberOfOperators;
+    }
+
+    public int getNumberOfOperators(){
+        return numberOfOperators;
+    }
+
+    public int getMaxDepth(){
+        countMaxCLDepth(-1,allCodeInline,false);
+        return maxDepth;
+    }
+
+    public void countOperators(){
+        CL = 0;
+        arithmeticOperatorCount = 0;
+        compoundAssignmentOperatorCount = 0;
+        comparisonOperatorCount = 0;
+        cyclicOperatorCount = 0;
+        otherOperatorCount = 0;
+        for(String currentString:stringsWithoutComments){
+                Matcher matchCL = patternCL.matcher(currentString);
+                Matcher matchArithmeticOperators = arithmeticOperatorPattern.matcher(currentString);
+                Matcher matchCompoundOperators = compoundAssignmentOperatorPattern.matcher(currentString);
+                Matcher matchBitwiseOperators = otherOperatorsPattern.matcher(currentString);
+                Matcher matchComparisonOperators = comparisonOperatorPattern.matcher(currentString);
+                Matcher matchCyclicOperators = cyclicOperatorPattern.matcher(currentString);
+                Matcher matchLogicalOperators = logicalOperatorPattern.matcher(currentString);
+
+                while(matchCL.find()){
+                    CL++;
+                }
+                while(matchArithmeticOperators.find()){
+                    arithmeticOperatorCount++;
+                }
+                while(matchCompoundOperators.find()){
+                    compoundAssignmentOperatorCount++;
+                }
+                while(matchComparisonOperators.find()){
+                    comparisonOperatorCount++;
+                }
+                while(matchLogicalOperators.find()){
+                    logicalOperatorCount++;
+                }
+                while(matchCyclicOperators.find()){
+                    cyclicOperatorCount++;
+                }
+                while(matchBitwiseOperators.find()){
+                    otherOperatorCount++;
+                }
+        }
+
+        numberOfOperators = CL+ arithmeticOperatorCount + compoundAssignmentOperatorCount+cyclicOperatorCount
+                +comparisonOperatorCount+logicalOperatorCount+ otherOperatorCount ;
+
+    }
+
+    public int getLastOvalBracePos(String substring){
+        Matcher findIfCL = ifCLMatchPattern.matcher(substring);
+        findIfCL.find();
+        int countOvalBrace = 0;
+        boolean ovalBraceTookPlace = false;
+        int i = 0;
+        for(i = findIfCL.start();i<substring.length();i++){
+            if(substring.charAt(i)=='('){
+                countOvalBrace++;
+                ovalBraceTookPlace = true;
+            }else if(substring.charAt(i)==')'){
+                countOvalBrace--;
+            }
+            if(ovalBraceTookPlace&&countOvalBrace==0){
+                break;
+            }
+        }
+        return i;
+    }
+
+    public int getFigureBracePos(String substring){
+        Matcher matchBrace = braceMatchPattern.matcher(substring);
+        matchBrace.find();
+        int i=0;
+        for(i=matchBrace.start();i<substring.length();i++){
+            if(substring.charAt(i)=='{'){
+                break;
+            }
+        }
+        return i;
+    }
+
+    public int getLastFigureBracePos(String substring){
+        Matcher matchBrace = braceMatchPattern.matcher(substring);
+        matchBrace.find();
+        int countFigureBrace = 0;
+        boolean figureBraceTookPlace = false;
+        int i = 0;
+        for(i = matchBrace.start();i<substring.length();i++){
+            if(substring.charAt(i)=='{'){
+                countFigureBrace++;
+                figureBraceTookPlace = true;
+            }else if(substring.charAt(i)=='}'){
+                countFigureBrace--;
+            }
+            if(figureBraceTookPlace&&countFigureBrace==0){
+                break;
+            }
+        }
+        return i;
+    }
+
+    public void countMaxCLDepth(int depth, String substring, boolean insideCL) {
+        Matcher matchIfCL = ifCLMatchPattern.matcher(substring);
+        Matcher matchBrace = braceMatchPattern.matcher(substring);
+
+        if (depth > maxDepth) {
+            maxDepth = depth;
+        }
+        if (insideCL) {
+            if (matchIfCL.matches()) {
+                int i = getLastOvalBracePos(substring);
+                countMaxCLDepth(depth + 1, substring.substring(i + 1), true);
+            } else if (matchBrace.matches()) {
+                int figureBracePos = getFigureBracePos(substring);
+                int lastFigureBracePos = getLastFigureBracePos(substring);
+                countMaxCLDepth(depth + 1, substring.substring(figureBracePos + 1, lastFigureBracePos - 1), true);
+                countMaxCLDepth(depth, substring.substring(lastFigureBracePos + 1), false);
+            }else{
+
+            }
+        } else {
+            if (matchIfCL.find()) {
+                int lastOvalBracePos = getLastOvalBracePos(substring);
+                countMaxCLDepth(depth, substring.substring(lastOvalBracePos + 1), true);
+            }
+        }
+    }
+}
---
+++ Metrology/Parser2.0(ForJava)/ParserGUI/src/sample/Metrics.java
@@ -0,0 +1,213 @@
+package sample;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Роман on 08.11.2014.
+ */
+public class Metrics {
+    class Comments{
+        public ArrayList<String> commentsList = new ArrayList<String>();
+        public ArrayList<Boolean> isComment = new ArrayList<Boolean>();
+
+        public Comments(){
+            commentsList.clear();
+            isComment.clear();
+        }
+
+        public void addComment(String currentString){
+            commentsList.add(currentString);
+            isComment.add(true);
+        }
+
+        public void markNonComment(){
+            isComment.add(false);
+        }
+
+    }
+    public final int USUAL_STRING = 0;
+    public final int INLINE_COMMENT = 1;
+    public final int BEGINNING_OF_MULTILINE_COMMENT = 2;
+    public final int INFINITY = 999;
+
+    private Pattern doubleSlashPattern = Pattern.compile("//");
+    private Pattern openingSlashPattern = Pattern.compile("/[*]");
+    private Pattern closingSlashPattern = Pattern.compile("[*]/");
+
+    protected Comments comments;
+
+    protected ArrayList<String> allStrings = new ArrayList<String>();
+    protected ArrayList<String> stringsWithoutComments = new ArrayList<String>();
+    protected ArrayList<String> sourceCodeStringsOnly = new ArrayList<String>();
+
+    public Metrics(){
+        allStrings = Controller.getStringsFromTextArea();
+        comments = getComments();
+        stringsWithoutComments = getStringsWithoutComments();
+        sourceCodeStringsOnly = getSourceCodeStringsOnly();
+    }
+
+    public ArrayList<String> getStringsWithoutComments(){
+        ArrayList<String> stringsNonComments = new ArrayList<String>();
+        stringsNonComments.clear();
+        String allCodeInline = Controller.textToAnalyse.getText();
+        String codeWithoutCommentsInline = removeComments(allCodeInline);
+        int startPos = 0;
+        for(int i = 0;i<codeWithoutCommentsInline.length();i++){
+            if((codeWithoutCommentsInline.charAt(i)=='\n')||(i==codeWithoutCommentsInline.length()-1)){
+                String stringToAdd = codeWithoutCommentsInline.substring(startPos,i);
+                if(!stringToAdd.isEmpty()){
+                    stringsNonComments.add(stringToAdd);
+                }else{
+                    stringsNonComments.add(String.valueOf(codeWithoutCommentsInline.charAt(i)));
+                }
+                startPos = i+1;
+            }
+        }
+        return stringsNonComments;
+    }
+
+    public static String removeCharAt(String s, int pos) {
+
+        return s.substring(0,pos)+" "+s.substring(pos+1);
+
+    }
+
+    private String removeComments(String sourceCode){
+        int typeOfComment = 0;
+        int i = 0;
+        while(i<sourceCode.length()){
+            if(typeOfComment==0){
+                if((sourceCode.charAt(i)=='/')&&(sourceCode.charAt(i+1)=='/')){
+                    typeOfComment=INLINE_COMMENT;
+                    sourceCode = removeCharAt(sourceCode,i);
+                    i++;
+                    sourceCode = removeCharAt(sourceCode,i);
+                    i++;
+                }else if((sourceCode.charAt(i)=='/')&&(sourceCode.charAt(i+1)=='*')){
+                    typeOfComment=BEGINNING_OF_MULTILINE_COMMENT;
+                    sourceCode = removeCharAt(sourceCode,i);
+                    i++;
+                    sourceCode = removeCharAt(sourceCode,i);
+                    i++;
+                }else{
+                    i++;
+                }
+            }else{
+                if((sourceCode.charAt(i)=='\n')){
+                    if(typeOfComment==INLINE_COMMENT) {
+                        typeOfComment = 0;
+                    }
+                    i++;
+                }else if((sourceCode.charAt(i)=='*')&&(sourceCode.charAt(i+1)=='/')){
+                    if(typeOfComment==BEGINNING_OF_MULTILINE_COMMENT){
+                        typeOfComment=0;
+                        sourceCode = removeCharAt(sourceCode,i);
+                        i++;
+                        sourceCode = removeCharAt(sourceCode,i);
+                        i++;
+                    }else{
+                        sourceCode = removeCharAt(sourceCode,i);
+                        i++;
+                    }
+                }else{
+                    sourceCode = removeCharAt(sourceCode,i);
+                    i++;
+                }
+
+            }
+        }
+        return sourceCode;
+    }
+
+    public ArrayList<String> getSourceCodeStringsOnly(){
+        ArrayList<String> sourceCodeStrings = new ArrayList<String>();
+        for(String currentString:stringsWithoutComments){
+            if(!((currentString.isEmpty()==true)||(currentString.matches("[ \t\n]{1,}")))){
+                sourceCodeStrings.add(currentString);
+            }
+        }
+        return sourceCodeStrings;
+    }
+
+    private boolean matchString(String stringToTest, String regEx){
+        Pattern pattern = Pattern.compile(regEx);
+        Matcher matcher = pattern.matcher(stringToTest);
+        return matcher.matches();
+    }
+
+    public int typeOfString(String testString) {
+        int doubleSlashFirstPos = INFINITY;
+        int openingSlashPos = INFINITY;
+        int closingSlashPos = INFINITY;
+
+        Matcher matchDoubleSlash = doubleSlashPattern.matcher(testString);
+        if (matchDoubleSlash.find()) {
+            doubleSlashFirstPos = matchDoubleSlash.start();
+        }
+        Matcher matchOpeningSlash = openingSlashPattern.matcher(testString);
+        while (matchOpeningSlash.find()) {
+            openingSlashPos = matchOpeningSlash.start();
+        }
+        Matcher matchClosingSlash = closingSlashPattern.matcher(testString);
+        while (matchClosingSlash.find()) {
+            closingSlashPos = matchClosingSlash.start();
+        }
+        if ((doubleSlashFirstPos < openingSlashPos) ||
+                ((openingSlashPos < closingSlashPos) && closingSlashPos != INFINITY)) {
+            return INLINE_COMMENT;
+        }else if (((openingSlashPos > closingSlashPos) && openingSlashPos != INFINITY) ||
+                (openingSlashPos < closingSlashPos) && closingSlashPos == INFINITY) {
+            return BEGINNING_OF_MULTILINE_COMMENT;
+        }else{
+            return 0;
+        }
+    }
+
+    public boolean hasCommentClosingSlash(String stringToMatch){
+        String commentClosingSlash = "(.)*[*]/(.)*";
+        if(matchString(stringToMatch,commentClosingSlash)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public Comments getComments(){
+        Comments comments = new Comments();
+        boolean multilineComment = false;
+        for(String currentString:allStrings){
+            if(multilineComment){
+                comments.addComment(currentString);
+                if(hasCommentClosingSlash(currentString)){
+                    if(typeOfString(currentString)==BEGINNING_OF_MULTILINE_COMMENT){
+                        multilineComment = true;
+                    }else{
+                        multilineComment = false;
+                    }
+                }
+            }else{
+                switch(typeOfString(currentString)){
+                    case INLINE_COMMENT:{
+                        comments.addComment(currentString);
+                        break;
+                    }
+                    case BEGINNING_OF_MULTILINE_COMMENT: {
+                        comments.addComment(currentString);
+                        multilineComment = true;
+                        break;
+                    }
+                    default:{
+                        comments.markNonComment();
+                    }
+                }
+            }
+        }
+        return comments;
+    }
+}
